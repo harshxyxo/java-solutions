4e39c087a1c9a2613c4603ff87dbfd127252bd58
Harsh Sharma <harshsharmarohta@gmail.com>
2026-09-08 15:20:02 +0530

    Add charwithmaxfreq class to find and display characters with maximum frequency

---
+++ string/charwithmaxfreq.java
@@ -0,0 +1,23 @@
+package string;
+
+public class charwithmaxfreq {
+    public static void main(String[] args){
+        String s = "mississippi";
+        int[] freq = new int[26];
+        for(int i=0; i<s.length();i++){
+            freq[s.charAt(i) -'a']++;
+        }
+       int max =0;
+       for(int i=0;i<26;i++){
+        if(freq[i]>max){
+            max =freq[i];
+        }
+       }
+       for(int i=0;i<26;i++){
+        if(freq[i] == max){
+            System.out.println((char)(i + 'a') + ": " + freq[i]);
+        }
+       }
+    }
+    
+}
